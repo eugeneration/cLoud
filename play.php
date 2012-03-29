d127c0688fcaaadee1b68c74cda2af978050963c
Ubuntu <ubuntu@ip-10-46-214-87.ec2.internal>
2012-03-29 01:56:42 +0000

    merged play.php by removing duplicated code

--- play.php
+++ play.php
@@ -6,11 +6,8 @@ $app_secret = '82d05a1198b87fe1350d';
 $app_id = '17464';
 
 $pusher = new Pusher($app_key, $app_secret, $app_id);
-<<<<<<< HEAD
-$data = array('message' => microtime());
-=======
-$data = array('message' => 'This is an HTML5 Realtime Push Notification!');
-/* , 'time'=>microtime(true) + 1); */
->>>>>>> 80e3db17c97bd2c4f060bab0a3ff6fcc3ae8dbbf
+
+$data = array('message' => microtime(true) + 1);
+
 $pusher->trigger('play_ey_ey', 'play', $data);
 ?>
